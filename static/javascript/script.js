f9a93e11a6ca42e6084985e3abfd175288e26327
cnimcheski <cnimcheski@hotmail.com>
2024-06-22 16:27:47 -0700

    Added scroll reveal feature and improved about page

---
+++ static/javascript/script.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 1000,
+    delay: 0,
+    reset: true
+});
+
+sr.reveal('.home-image-overlay-container', {origin: 'top'})
+sr.reveal('.header-image-overlay', {origin: 'top'})
